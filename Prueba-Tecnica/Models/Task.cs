6d033c188def0cde767e5f0124fca760f7616c23
BMarcano <brayanmarcanor@gmail.com>
2023-08-09 17:40:46 -0300

    Add project files.

---
+++ Prueba-Tecnica/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba_Tecnica.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
